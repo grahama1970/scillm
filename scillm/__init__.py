04162f3fd24495034bdb33d469dc57cf7419c6d6
grahama1970 <graham@grahama.co>
2025-10-06 09:31:38 -0400

    contrib.batch: add JsonlCheckpoint + TokenBucket helpers; tests; scillm re-exports contrib; docs: batch_helpers

---
+++ scillm/__init__.py
@@ -0,0 +1,3 @@
+# Re-export litellm surface for convenience when installed as 'scillm'.
+from litellm import *  # noqa: F401,F403
+
---
+++ scillm/contrib/__init__.py
@@ -0,0 +1,3 @@
+# Bridge to contrib helpers under litellm.contrib
+from litellm.contrib.batch import JsonlCheckpoint, TokenBucket, token_bucket_from_env  # noqa: F401
+
